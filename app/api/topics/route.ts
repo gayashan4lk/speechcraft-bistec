b266bea7c378b9edbae2bd42a0a98b32882f0c50
gayashan4lk <gayashan4lk@gmail.com>
2023-08-03 08:17:15 +0530

    build backend

---
+++ app/api/topics/route.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import topics from '../../../data/speech-topics.json';
+
+export async function GET(request: Request, {params}: any) {
+    try{
+        const nextId = topics.length - 1;
+        const filePath = path.join(process.cwd(), "data", "speech-topics-data.txt");
+        const textContent = fs.readFileSync(filePath, 'utf-8');
+        const textLines = textContent.split('\n');
+
+        const textTopics: typeof topics = []
+        textLines.forEach((line) => {
+            const [_id, _topic] = line.split('. ', 2);
+            const id = parseInt(_id) + nextId;
+            const topic: {id: number, topic: string} = {
+                id : id,
+                topic: `${_topic}`
+            }
+          textTopics.push(topic);
+        })
+
+        const result= topics.concat(textTopics);
+        return new Response(JSON.stringify(result), {
+            status: 200,
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+    } catch(error){
+        console.error('Error reading the file: ', error);
+        return new Response(JSON.stringify({error: "Error reading the file"}), {
+            status: 500,
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+    }
+}
