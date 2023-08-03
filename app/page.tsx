6077a844fd1b08743571c4866e1ec8489a8dcd24
gayashan4lk <gayashan4lk@gmail.com>
2023-08-03 08:25:01 +0530

    update base url

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import AppContainer from "@/components/AppContainer";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/';
 async function getTopics(): Promise<{id: number, topic: string}[]> {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/topics`);
+	const res = await fetch(`${baseUrl}api/topics`);
 
 	if(!res.ok){
 		throw new Error('Failed to fetch topics.');
