17661ecb012e19dab38315d8f4d130123d83776e
gayashan4lk <gayashan4lk@gmail.com>
2025-07-04 11:38:42 +0530

    update import path to use cartoon speech topics data

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useRef, useEffect } from "react";
 import { formatTime } from "@/utils/time-util-fns";
 import { timeConfig } from "@/data/time-config";
-import data from "../data/speech-topics-mothers-day.json";
+import data from "../data/speech-topics-cartoon.json";
 import { TitleBar } from "@/components/TitleBar";
 import { GoButton } from "@/components/GoButton";
 
