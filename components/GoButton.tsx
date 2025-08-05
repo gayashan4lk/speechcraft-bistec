8e51f433e4e511da12cc48d32b86fad6ad2fb4a7
gayashan4lk <gayashan4lk@gmail.com>
2025-08-05 21:33:20 +0530

    update GoButton component to change button text from 'GO' to 'START'

--- components/GoButton.tsx
+++ components/GoButton.tsx
@@ -16,7 +16,7 @@ export function GoButton({
 			className='p-5 border-2 border-slate-300 bg-slate-200 rounded-full w-40 h-20 hover:border-3 hover:border-slate-400 hover:bg-slate-300'
 		>
 			<span className='text-2xl font-bold text-slate-700'>
-				{isSpeechInProgress ? `STOP` : `GO`}
+				{isSpeechInProgress ? `STOP` : `START`}
 			</span>
 		</button>
 	);
