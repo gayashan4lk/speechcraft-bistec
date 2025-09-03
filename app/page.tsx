90a70014597b53f8d10b86af6f8b9669e63ee32a
gayashan4lk <gayashan4lk@gmail.com>
2025-09-03 15:49:42 +0530

    update import path to use environment speech topics and add new topics

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useRef, useEffect } from "react";
 import { formatTime } from "@/utils/time-util-fns";
 import { timeConfig } from "@/data/time-config";
-import data from "../data/speech-topics-youth-day.json";
+import data from "../data/speech-topics-environment.json";
 import { TitleBar } from "@/components/TitleBar";
 import { GoButton } from "@/components/GoButton";
 
